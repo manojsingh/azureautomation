0625bacda76bd7b56c00b289ce648d3538ea7c3f
manojs@microsoft.com <manojs@microsoft.com>
2020-06-03 00:06:16 -0400

    Added tar file

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -4,9 +4,14 @@
 cd /tmp
 sudo wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo apt-get install ./packages-microsoft-prod.deb
-sudo apt update
-sudo apt install –force-yes –yes powershell
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y install powershell
 
+# download application
+sudo wget https://raw.github.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/music-store-azure-demo-pub.tar /
+sudo mkdir /opt/demoapp
+sudo tar -xf demo-app.tar -C /opt/demoapp
 
 
 # install nodejs
@@ -19,16 +24,11 @@ sudo apt-get install -y dotnet-dev-1.0.0-preview2-003121
 sudo apt-get install -y nginx
 sudo service nginx start
 sudo touch /etc/nginx/sites-available/default
-sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/nginx-config/default -O /etc/nginx/sites-available/default
-sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
+sudo wget https://raw.githubusercontent.com/manojsingh/azureautomation/master/demoapp/nginx-config/default -O /etc/nginx/sites-available/default
+sudo cp /opt/demoapp/nginx-config/default /etc/nginx/sites-available/
 sudo nginx -s reload
 
-# download and run application
-sudo wget https://raw.github.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/music-store-azure-demo-pub.tar /
-sudo mkdir /opt/demoapp
-sudo tar -xf demo-app.tar -C /opt/demoapp
-cd demoapp
-node 
+
 
 # Populate DB for Demo
 /opt/demoapp/populatedb.sh &
